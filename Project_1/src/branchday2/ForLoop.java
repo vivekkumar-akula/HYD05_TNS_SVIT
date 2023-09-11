1494345dda20276e84cc81b0b0b501989a372762
91990 <vivekkumarakula99@gmail.com>
2023-09-11 14:14:52 +0530

    commit

---
+++ Project_1/src/branchday2/ForLoop.java
@@ -0,0 +1,13 @@
+package branchday2;
+
+public class ForLoop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+
+}
